484215e2d89a192ba4cb46b4231817197dadad65
abhi250802 <abhiiit1234@gmail.com>
2024-03-01 12:33:44 +0530

    ip

--- server/controllers/sendMail.js
+++ server/controllers/sendMail.js
@@ -1,9 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async(req, res) => {
+    const ipAddress = req.ip;
     let testAccount = await nodemailer.createTestAccount();
 let transporter = await nodemailer.createTransport({
     host: "smtp.ethereal.email",
+    // host: "gmail",
     port: 587,
     auth: {
         user: 'viva.macejkovic@ethereal.email',
@@ -15,8 +17,8 @@ let info = await transporter.sendMail({
     from: '"abhishek 👻" <abhiiit1234@gmail.com>', // sender address
     to: "at9188@srmist.edu.in", // list of receivers
     subject: "reporting the image", // Subject line
-    text: "the image is inappropriate", // plain text body
-    html: "<b>the image is inappropriate</b>", // html body
+    text: `the image is inappropriate found on ip address ${ipAddress}`, // plain text body
+    html: `<b>the image is inappropriaten ${ipAddress}</b>`, // html body
   });
     res.send('Email sent successfully');};
 
